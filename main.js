6aa242d01bc0fb2a2df487c4c3adc968ded0b3bc
Valentina Villa <ovejod12@hotmail.com>
2023-05-26 23:38:49 -0500

    Acoplamiento del menu mobile y creacion de @media querys para responsive

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const imagenMenu = document.querySelector('.menu');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+imagenMenu.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
