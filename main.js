4f847b407a725485349997bb4f03c72f5186e75c
Valentina Villa <ovejod12@hotmail.com>
2023-05-27 07:55:48 -0500

    Creacion de funcion para el aside del shopping cart

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const imagenMenu = document.querySelector('.menu');
+const menuOptions = document.querySelector('.menu');
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
+menuOptions.addEventListener('click', toggleMobileMenu);
+shoppingCart.addEventListener('click', toggleCarritoAside);
+
+
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
-}
+};
 
-imagenMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
-}
+};
+
+function toggleCarritoAside() {
+    aside.classList.toggle('inactive');
+};
+    
