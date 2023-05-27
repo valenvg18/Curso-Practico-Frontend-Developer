c19f2df08201825f81734d9e4e8700dcfe1e5ce8
Valentina Villa <ovejod12@hotmail.com>
2023-05-27 10:16:40 -0500

    Se inserto el ciclo dentro de una funcion para llamarla e insertar la lista de productos

--- main.js
+++ main.js
@@ -74,46 +74,50 @@ productList.push({
 
 
 
-for (product of productList) {
+function renderProducts(array) {
+    for (product of array) {
 
-    const productCard = document.createElement('div');
-    productCard.classList.add('product-card');
-
-    const productImg = document.createElement('img');
-    // product = {name, price, image} --> product.image
-    productImg.setAttribute('src', product.image);
-
-    const productInfo = document.createElement('div');
-    productInfo.classList.add('product-info');
-
-    const productInfoDiv = document.createElement('div');
-
-    const productPrice = document.createElement('p');
-    productPrice.innerText = '$' + product.price;
-
-    const productName = document.createElement('p');
-    productName.innerText = product.name;
-
-    // Aqui estamos agregando el precio y el nombre del producto a la etiqueta div
-    productInfoDiv.appendChild(productPrice);
-    productInfoDiv.appendChild(productName);
-
-    const productInfoFigure = document.createElement('figure');
-
-    const productImgCart = document.createElement('img');
-    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
-
-    // Aqui estamos agregando la imagen del icono del carrito a la etiqueta figure
-    productInfoFigure.appendChild(productImgCart);
-
-    // Aqui estamos agregando el div que contiene el precio y el nombre, y el figure dentro del div product-info
-    productInfo.appendChild(productInfoDiv);
-    productInfo.appendChild(productInfoFigure);
-
-    // Aqui estamos agregando la imagen y el div product-info dentro del div product-card
-    productCard.appendChild(productImg);
-    productCard.appendChild(productInfo);
-
-    cardsContainer.appendChild(productCard);
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        // product = {name, price, image} --> product.image
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        // Aqui estamos agregando el precio y el nombre del producto a la etiqueta div
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+    
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        // Aqui estamos agregando la imagen del icono del carrito a la etiqueta figure
+        productInfoFigure.appendChild(productImgCart);
+    
+        // Aqui estamos agregando el div que contiene el precio y el nombre, y el figure dentro del div product-info
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        // Aqui estamos agregando la imagen y el div product-info dentro del div product-card
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
 }
+
+renderProducts(productList);
 
